b54c30d7fa0574e29757e79d8af731137fe49ee1
ZhannaKhizrieva <zhanna-apteka@mail.ru>
2024-07-07 13:46:52 +0300

    add App.modules.css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
 
 export const App = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		fetch('https://mocki.io/v1/28454519-5809-41e6-8751-03d2a479fdd8')
+			.then((loadedData) => loadedData.json())
+			.then((loadedProducts) => {
+				setProducts(loadedProducts);
+			});
+	}, []);
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div className={styles.App}>
+			{products.map(({ id, name, price }) => (
+				<div кеу={id}>
+					{name} - {price} руб{' '}
+				</div>
+			))}
 		</div>
 	);
 };
